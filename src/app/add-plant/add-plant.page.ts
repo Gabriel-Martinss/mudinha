148ab93d74e705cca6d30f9e0a6fa0900b7db894
Gabriel <gb.magamartins19@gmail.com>
2025-10-01 20:05:20 -0300

    chore: first commit of the project

---
+++ src/app/add-plant/add-plant.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-plant',
+  templateUrl: './add-plant.page.html',
+  styleUrls: ['./add-plant.page.scss'],
+})
+export class AddPlantPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'plants',
+    loadChildren: () => import('./plants/plants.module').then( m => m.PlantsPageModule)
+  },
+  {
+    path: 'marketplace',
+    loadChildren: () => import('./marketplace/marketplace.module').then( m => m.MarketplacePageModule)
+  },
+  {
+    path: 'care',
+    loadChildren: () => import('./care/care.module').then( m => m.CarePageModule)
+  },
+  {
+    path: 'sign-up',
+    loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
+  },
+  {
+    path: 'recover',
+    loadChildren: () => import('./recover/recover.module').then( m => m.RecoverPageModule)
+  },
+  {
+    path: 'add-plant',
+    loadChildren: () => import('./add-plant/add-plant.module').then( m => m.AddPlantPageModule)
+  },
+  {
+    path: 'personal-info',
+    loadChildren: () => import('./personal-info/personal-info.module').then( m => m.PersonalInfoPageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
