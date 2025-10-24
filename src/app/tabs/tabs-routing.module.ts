085572766eb44720635721ce982381fc4acfed36
Gabriel <gb.magamartins19@gmail.com>
2025-10-23 22:08:25 -0300

    fix: Each page is now completely separate with its own HTML and SCSS files, and the tab navigation will correctly switch between them

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,27 +8,35 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'plants',
+        loadChildren: () => import('../plants/plants.module').then(m => m.PlantsPageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'care',
+        loadChildren: () => import('../care/care.module').then(m => m.CarePageModule)
+      },
+      {
+        path: 'marketplace',
+        loadChildren: () => import('../marketplace/marketplace.module').then(m => m.MarketplacePageModule)
+      },
+      {
+        path: 'personal-info',
+        loadChildren: () => import('../personal-info/personal-info.module').then(m => m.PersonalInfoPageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home',
     pathMatch: 'full'
   }
 ];
